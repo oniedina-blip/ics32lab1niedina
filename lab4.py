76a2dcac49662ccc3773d7d70fca107ab9777265
oniedina-blip <oniedina@uci.edu>
2026-04-20 20:02:06 -0700

    lab4.py

---
+++ lab4.py
@@ -0,0 +1,129 @@
+"""
+This program enables a user to input short one line notes and have them stored in a file called pynote.txt
+"""
+
+from pathlib import Path
+
+NOTES_PATH = "."
+NOTES_FILE = "pynote.txt"
+
+def is_int(val):
+    try:
+        int(val)
+        return True
+    except ValueError:
+        return False
+
+
+def save_note(note: str):
+    p = Path(NOTES_PATH) / NOTES_FILE
+
+    if not p.exists():
+        p.touch(exist_ok=True)
+    
+    f = p.open('a')
+    f.write(note + '\n')
+    f.close()
+
+
+def read_notes():
+    p = Path(NOTES_PATH) / NOTES_FILE
+
+    if not p.exists():
+        return
+    
+    print("Here are your notes: \n")
+
+    f = p.open()
+    for line in f:
+        print(line)
+    f.close()
+
+
+def remove_note() -> str:
+    p = Path(NOTES_PATH) / NOTES_FILE
+
+    if not p.exists():
+        """
+        REQUIREMENT 3 - Should be very similar, custom message not required.
+        - remove the return line and uncomment the raise line to complete 3.
+        """
+        return                     # <bla line>
+        # raise FileNotFoundError # <bla2 line>
+      
+
+    print("Here are your notes: \n")
+
+    f = p.open()
+    id = 1
+    lines = []
+
+    for line in f:
+        lines.append(line)
+        print(f"{id}: {line}")
+        id = id + 1
+    f.close()
+
+    remove_id = input("Enter the number of the note you would like to remove: ")
+
+    if not is_int(remove_id):
+        print("Not a valid number, cancelling operation.")
+        return ""
+
+    f = p.open('w')
+    id = 1
+    removed_note = ""
+
+    for line in lines:
+        if id == int(remove_id):
+            removed_note = line
+          
+        else:
+            f.write(line)
+        id = id + 1
+    f.close()
+
+    return removed_note
+
+
+# REQ-1 :: is_int tests
+assert is_int(5) == True
+assert is_int("10") == True
+assert is_int("-7") == True
+assert is_int("abc") == False
+assert is_int(3.14) == False
+assert is_int(None) == False
+
+
+# REQ-2 :: remove_note test
+try:
+    Path(NOTES_PATH, NOTES_FILE).unlink()
+except FileNotFoundError:
+    pass
+
+try:
+    remove_note()
+    assert False
+  
+except FileNotFoundError:
+    assert True
+
+
+def run():
+    note = input("Please enter a note (enter :d to delete a note or :q to exit):  ")
+    if note == ":d":
+        note = remove_note()
+        print(f"The following note has been removed: \n\n {note}")
+    elif note == ":q":
+        return
+    else:
+        save_note(note)
+    run()
+
+
+if __name__ == "__main__":
+    print("Welcome to PyNote! \n")
+    read_notes()
+
+    run()
+    
