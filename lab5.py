85defb920d99127d73bb7becddf5bed83a4f32f2
oniedina-blip <oniedina@uci.edu>
2026-05-04 20:47:14 -0700

    Create lab5.py

---
+++ lab5.py
@@ -0,0 +1,93 @@
+# lab5.py
+
+# Starter code for lab 5 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+# OLEKSANDRA
+# oniedina
+# STUDENT ID
+
+# ---------------------
+# Write your Note class here
+# ---------------------
+from pathlib import Path
+
+class Note:
+    def __init__(self, path: Path):
+        self._path = path
+
+  
+    def read_notes(self) -> list[str]:
+        notes = []
+        with open(self._path, "r") as f:
+            for line in f:
+                notes.append(line.strip())
+        return notes
+
+  
+    def save_note(self, note: str) -> None:
+        with open(self._path, "a") as f:
+            f.write(note + "\n")
+
+  
+    def remove_note(self, index: int) -> str:
+        notes = self.read_notes()
+
+        if index < 0 or index >= len(notes):
+            raise ValueError("Invalid index")
+
+        removed_note = notes.pop(index)
+
+        with open(self._path, "w") as f:
+            for n in notes:
+                f.write(n + "\n")
+
+        return removed_note
+
+# ---------------------------------------------------------
+# DO NOT CHANGE ANY CODE BELOW THIS LINE 
+# ---------------------------------------------------------
+
+NOTES_PATH = "."
+NOTES_FILE = "pynote.txt"
+
+def print_notes(notes:list[str]):
+    id = 0
+    for n in notes:
+        print(f"{id}: {n}")
+        id+=1
+
+def delete_note(note:Note):
+    try:
+        remove_id = input("Enter the number of the note you would like to remove: ")
+        remove_note = note.remove_note(int(remove_id))
+        print(f"The following note has been removed: \n\n {remove_note}")
+    except FileNotFoundError:
+        print("The PyNote.txt file no longer exists")
+    except ValueError:
+        print("The value you have entered is not a valid integer")
+
+def run():
+    p = Path(NOTES_PATH) / NOTES_FILE
+    if not p.exists():
+        p.touch()
+    note = Note(p)
+    
+    print("Here are your notes: \n")
+    print_notes(note.read_notes())
+
+    user_input = input("Please enter a note (enter :d to delete a note or :q to exit):  ")
+
+    if user_input == ":d":
+        delete_note(note)
+    elif user_input == ":q":
+        return
+    else:    
+        note.save_note(user_input)
+    run()
+
+
+if __name__ == "__main__":
+    print("Welcome to PyNote! \n")
+    run()
+  
